89b9681c3c412c25641cae2d9d5d2018c6481fd3
Marcelo Marques <marcelo.lima@inova100.com.br>
2026-07-08 14:39:07 -0300

    trocando app kotelin

--- frontend-app/build.gradle.kts
+++ frontend-app/build.gradle.kts
@@ -1,5 +1,37 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+	kotlin("jvm") version "2.3.21"
+	kotlin("plugin.spring") version "2.3.21"
+	id("org.springframework.boot") version "4.1.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.frontend-app"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
---
+++ frontend-app/src/main/kotlin/com/frontend_app/frontend_app/FrontendAppApplication.kt
@@ -0,0 +1,11 @@
+package com.frontend_app.frontend_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrontendAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrontendAppApplication>(*args)
+}
